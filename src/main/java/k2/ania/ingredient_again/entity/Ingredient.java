d688c9dc9d04bd3c4b0e5352dd93f5c9d22f2b23
Ania-su <antsaaa.randria@gmail.com>
2026-03-30 19:33:11 +0300

    feat: ingredient entity

--- src/main/java/k2/ania/ingredient_again/entity/Ingredient.java
+++ src/main/java/k2/ania/ingredient_again/entity/Ingredient.java
@@ -1,4 +1,28 @@
 package k2.ania.ingredient_again.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+
 public class Ingredient {
+
+    public enum CategoryEnum {
+        VEGETABLE,
+        ANIMAL,
+        MARINE,
+        DIARY,
+        OTHER
+    }
+
+    private int id;
+    private String name;
+    private Double price;
+    private CategoryEnum category;
+    private List<StockMovement> stockMovementList;
 }
